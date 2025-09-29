f70b6249d357a84be92cb4223b963cf5b16b8ecd
ChengyangJiang <12433001@mail.sustech.edu.cn>
2025-09-29 13:11:28 +0800

    Delete Synthetic_Quadratic_Optimization/generate_ring_graph.m

--- Synthetic_Quadratic_Optimization/generate_ring_graph.m
+++
@@ -1,9 +0,0 @@
-function A = generate_ring_graph(n)
-    % Generate adjacency matrix of a ring graph with n clients
-    
-    A = zeros(n);                 % initialize adjacency matrix
-    for i = 1:n
-        A(i, mod(i, n) + 1) = 1;  % connect client i to i+1
-        A(mod(i, n) + 1, i) = 1;  % ensure symmetry (undirected graph)
-    end
-end
