d372f3bc61a96409162835b77c9327380f7d4501
ChengyangJiang <12433001@mail.sustech.edu.cn>
2025-09-29 13:10:24 +0800

    Delete Synthetic_Quadratic_Optimization/generate_fully_graph.m

--- Synthetic_Quadratic_Optimization/generate_fully_graph.m
+++
@@ -1,9 +0,0 @@
-function A = generate_fully_graph(n)
-    % Generate adjacency matrix of a fully connected graph with n clients
-
-    % initialize n×n matrix with ones
-    A = ones(n);
-
-    % remove self-connections (set diagonal to zero)
-    A(1:n+1:end) = 0;
-end
