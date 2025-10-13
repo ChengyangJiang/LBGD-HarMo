d6bc568d6a15886cb8c6df63a2d37c499db240cf
ChengyangJiang <12433001@mail.sustech.edu.cn>
2025-10-13 17:45:14 +0800

    Create LBGD_HarMo_Quantize.py

---
+++ Logistic_Regression/LBGD_HarMo_Quantize.py
@@ -0,0 +1,8 @@
+def LBGD_HarMo_Quantize(x, m1, m2):
+    K = 2 ** (m1 - 1)           
+    Delta = 2 ** (-m2)          
+    half = 0.5 * Delta
+    y = np.clip(x, -K + half, K - half)
+    q = np.sign(y) * Delta * (np.floor(np.abs(y) / Delta) + 0.5)
+    q = np.clip(q, -K + half, K - half)
+    return q
