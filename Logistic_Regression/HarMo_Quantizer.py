5436fb5969a41dda66ebe3fa0bdc00d2dc3549e2
ChengyangJiang <12433001@mail.sustech.edu.cn>
2025-10-14 10:32:58 +0800

    Update HarMo_Quantizer.py

--- Logistic_Regression/HarMo_Quantizer.py
+++ Logistic_Regression/HarMo_Quantizer.py
@@ -1,8 +1,20 @@
-def HarMo_Quantizer(x, m1, m2):
+def HarMo_Quantizer(X, m1, m2):
+
+    X = np.asarray(X)
+    d, n = X.shape
+    Y = np.zeros((d, n))
+
+    # Quantization step
     K = 2 ** (m1 - 1)           
     Delta = 2 ** (-m2)          
     half = 0.5 * Delta
-    y = np.clip(x, -K + half, K - half)
-    q = np.sign(y) * Delta * (np.floor(np.abs(y) / Delta) + 0.5)
-    q = np.clip(q, -K + half, K - half)
+
+    for j in range(n):
+        x = X[:, j]
+        y = np.clip(x, -K + half, K - half)
+        q_col = np.sign(y) * Delta * (np.floor(np.abs(y) / Delta) + 0.5)
+        q_col = np.clip(q_col, -K + half, K - half)
+        Y[:, j] = q_col
+
+    q = Y
     return q
